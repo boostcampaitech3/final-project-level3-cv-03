dd3bf94feb8befa91a71306ce4eada6f97b11f74
kdg1993 <daegunkim0425@gmail.com>
2022-05-31 21:59:27 +0900

    [web] 사용자 업로드 사진의 얼굴 수 기반 로직 추가
    사용자가 업로드한 사진에 얼굴이 없거나 2개 이상인 경우 에러를 반환하는
    로직을 추가했습니다.
    얼굴 detection의 경우 models/efficientnet/efficientnet_model.py의
    transform_image 내부 구조를 수정하여 해결했습니다.
    
    transform_image 함수의 output은 특정 조건이 추가 되지 않으면 기존과 같아
    다른 구조에 문제가 없으며 테스트시 작동에 문제가 생기지 않았습니다.

--- Web_Part/front_streamlit/app.py
+++ Web_Part/front_streamlit/app.py
@@ -12,6 +12,8 @@ import webbrowser, json
 
 import front_streamlit.external_components as ec
 
+from models.efficientnet import efficientnet_model
+
 def uploaded_file_change_callback():
     for k in ['classification_done', 'apply_beautyGAN', 'user_face_confirm']:
         st.session_state[k] = False
@@ -53,113 +55,122 @@ def main():
 
     # If get user image
     if uploaded_file:
-        # Show user uploaded image
         image_bytes = uploaded_file.getvalue()
         no_makeup = transform_image(image_bytes)
         files = [
             ('files',(uploaded_file.name, image_bytes, uploaded_file.type))
             ]
-
-
-        with col2:
-            st.markdown(ec.template_subheading('업로드한 이미지', 'black', '#AED6F1', 1.8), unsafe_allow_html=True)
-            st.image(uploaded_file, use_column_width=True)
-            
-            face_detected = True
-
-            # Face detection branch (fit for col2)
-            if face_detected:
-                st.markdown(ec.template_subheading('박스 안쪽에 얼굴이 잘 위치해있나요?', 'black', '#AED6F1', '1.3'), unsafe_allow_html=True)
-            else:
-                st.error('''죄송합니다. 얼굴을 찾을 수 없습니다. 다른 이미지를 업로드해 주세요''')
         
-        # Face detection main branch
-        if face_detected:
-            _, sub_col1, sub_col2, _ = st.columns([2, 1, 1, 4])
-            with sub_col1:
-                user_face_true_btn = st.button('네')
-            with sub_col2:
-                user_face_false_btn = st.button('아니오') 
+        # Detect faces in the uploaded file
+        rslt, boxes = efficientnet_model.transform_image(image_bytes, True)
+        num_faces = len(boxes)
+        st.write(len(boxes))
+        
+        # Number of face check branch
+        if num_faces != 1:
+            with col2:
+                st.error('''죄송합니다. 사진에서 얼굴을 찾을 수 없거나 2명 이상의 얼굴이 존재합니다. 다른 이미지를 업로드해 주세요''')
+        else:
+            # Show user uploaded image
+            with col2:
+                st.markdown(ec.template_subheading('업로드한 이미지', 'black', '#AED6F1', 1.8), unsafe_allow_html=True)
+                st.image(uploaded_file, use_column_width=True)
+                
+                face_detected = True
 
-            # If the detected face is found to be inaccurate by the user
-            if user_face_false_btn:
-                st.session_state.user_face_confirm = False
-                st.session_state.apply_beautyGAN = False
-                st.session_state.classification_done = False
-
-                _, col2, col3, _ = st.columns(4)
-                with col2:
-                    st.markdown(ec.template_subheading('죄송합니다. 다른 사진을 업로드해주세요.', 'white', '', '1.2'), unsafe_allow_html=True)
+                # Face detection branch (fit for col2)
+                if face_detected:
+                    st.markdown(ec.template_subheading('박스 안쪽에 얼굴이 잘 위치해있나요?', 'black', '#AED6F1', '1.3'), unsafe_allow_html=True)
+                else:
+                    st.error('''죄송합니다. 얼굴을 찾을 수 없습니다. 다른 이미지를 업로드해 주세요''')
             
-            # If the detected face is found to be accurate by the user
-            if user_face_true_btn:
-                st.session_state.user_face_confirm = True
+            # Face detection main branch
+            if face_detected:
+                _, sub_col1, sub_col2, _ = st.columns([2, 1, 1, 4])
+                with sub_col1:
+                    user_face_true_btn = st.button('네')
+                with sub_col2:
+                    user_face_false_btn = st.button('아니오') 
 
-            if st.session_state.user_face_confirm:
-                # Get similar actor result and beautyGAN result
-                with col3:
-                    if not st.session_state.classification_done:
-                        with st.spinner('당신과 닮은 배우를 찾는 중 입니다...'):
-                            response_actor = requests.post("http://localhost:8008/actorclass", files=files)
-                            st.session_state.sim_actor_nm = response_actor.json()['name']
-                            st.session_state.classification_img = convert_bytes_to_image(response_actor.json()['ref_actor'])
-                            # beautyGAN에 보내줄 refer image 추가
-                            actor_to_bytes = base64.b64decode(response_actor.json()['ref_actor'])
-                            files.append(('files',(uploaded_file.name, actor_to_bytes, uploaded_file.type)))
-                                                    
-                            st.session_state.classification_done = True
+                # If the detected face is found to be inaccurate by the user
+                if user_face_false_btn:
+                    st.session_state.user_face_confirm = False
+                    st.session_state.apply_beautyGAN = False
+                    st.session_state.classification_done = False
 
-                            # TODO: Get beautyGAN result
-                            response = requests.post("http://localhost:8008/beauty", files=files)
-                            output_img = response.json()["result"]
-                        
-                            # ASCII코드로 변환된 bytes 데이터(str) -> bytes로 변환 -> 이미지로 디코딩
-                            bytes_list = list(map(lambda x: base64.b64decode(x), output_img))
-                            image_list = list(map(lambda x: Image.open(io.BytesIO(x)), bytes_list))
+                    _, col2, col3, _ = st.columns(4)
+                    with col2:
+                        st.markdown(ec.template_subheading('죄송합니다. 다른 사진을 업로드해주세요.', 'white', '', '1.2'), unsafe_allow_html=True)
+                
+                # If the detected face is found to be accurate by the user
+                if user_face_true_btn:
+                    st.session_state.user_face_confirm = True
 
-                            # Put beautyGAN result into the session state
-                            st.session_state.beautyGAN_img_list = image_list
+                if st.session_state.user_face_confirm:
+                    # Get similar actor result and beautyGAN result
+                    with col3:
+                        if not st.session_state.classification_done:
+                            with st.spinner('당신과 닮은 배우를 찾는 중 입니다...'):
+                                response_actor = requests.post("http://localhost:8008/actorclass", files=files)
+                                st.session_state.sim_actor_nm = response_actor.json()['name']
+                                st.session_state.classification_img = convert_bytes_to_image(response_actor.json()['ref_actor'])
+                                # beautyGAN에 보내줄 refer image 추가
+                                actor_to_bytes = base64.b64decode(response_actor.json()['ref_actor'])
+                                files.append(('files',(uploaded_file.name, actor_to_bytes, uploaded_file.type)))
+                                                        
+                                st.session_state.classification_done = True
 
+                                # TODO: Get beautyGAN result
+                                response = requests.post("http://localhost:8008/beauty", files=files)
+                                output_img = response.json()["result"]
+                            
+                                # ASCII코드로 변환된 bytes 데이터(str) -> bytes로 변환 -> 이미지로 디코딩
+                                bytes_list = list(map(lambda x: base64.b64decode(x), output_img))
+                                image_list = list(map(lambda x: Image.open(io.BytesIO(x)), bytes_list))
+
+                                # Put beautyGAN result into the session state
+                                st.session_state.beautyGAN_img_list = image_list
+
+                                # Show similar actor image
+                                st.markdown(ec.template_subheading(f'당신과 닮은 배우는 {st.session_state.sim_actor_nm}!', 'black', '#D5DBDB', 1.8),
+                                            unsafe_allow_html=True)
+                                st.image(st.session_state.classification_img, use_column_width=True)
+                                apply_beautyGAN_btn = st.button('메이크업 따라해보기')
+                                if apply_beautyGAN_btn:
+                                    st.session_state.apply_beautyGAN = True
+                        else:                        
                             # Show similar actor image
                             st.markdown(ec.template_subheading(f'당신과 닮은 배우는 {st.session_state.sim_actor_nm}!', 'black', '#D5DBDB', 1.8),
-                                        unsafe_allow_html=True)
+                                                                unsafe_allow_html=True)
                             st.image(st.session_state.classification_img, use_column_width=True)
                             apply_beautyGAN_btn = st.button('메이크업 따라해보기')
                             if apply_beautyGAN_btn:
                                 st.session_state.apply_beautyGAN = True
-                    else:                        
-                        # Show similar actor image
-                        st.markdown(ec.template_subheading(f'당신과 닮은 배우는 {st.session_state.sim_actor_nm}!', 'black', '#D5DBDB', 1.8),
-                                                            unsafe_allow_html=True)
-                        st.image(st.session_state.classification_img, use_column_width=True)
-                        apply_beautyGAN_btn = st.button('메이크업 따라해보기')
-                        if apply_beautyGAN_btn:
-                            st.session_state.apply_beautyGAN = True
 
-        if st.session_state.user_face_confirm and st.session_state.apply_beautyGAN:
-            # TODO: 이미지 View
-            image_bytes = uploaded_file.getvalue() # binary 형식
-            ref_bytes = st.session_state.classification_img 
+            if st.session_state.user_face_confirm and st.session_state.apply_beautyGAN:
+                # TODO: 이미지 View
+                image_bytes = uploaded_file.getvalue() # binary 형식
+                ref_bytes = st.session_state.classification_img 
 
-            def crop_face(image_raw):
-                return image_box
+                def crop_face(image_raw):
+                    return image_box
 
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
 
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col2:
-                st.image(no_makeup, use_column_width=True)
-                st.markdown(ec.template_subheading('당신의 얼굴', 'white', '', 1.2), unsafe_allow_html=True)
-            with col3:
-                st.image(st.session_state.beautyGAN_img_list[0], use_column_width=True)
-                st.markdown(ec.template_subheading('배우 메이크업 적용', 'white', '', 1.2), unsafe_allow_html=True)
-            with col4:
-                st.image(st.session_state.beautyGAN_img_list[1], use_column_width=True)
-                st.markdown(ec.template_subheading('배우의 얼굴', 'white', '', 1.2), unsafe_allow_html=True)
-                
+                col1, col2, col3, col4, col5 = st.columns(5)
+                with col2:
+                    st.image(no_makeup, use_column_width=True)
+                    st.markdown(ec.template_subheading('당신의 얼굴', 'white', '', 1.2), unsafe_allow_html=True)
+                with col3:
+                    st.image(st.session_state.beautyGAN_img_list[0], use_column_width=True)
+                    st.markdown(ec.template_subheading('배우 메이크업 적용', 'white', '', 1.2), unsafe_allow_html=True)
+                with col4:
+                    st.image(st.session_state.beautyGAN_img_list[1], use_column_width=True)
+                    st.markdown(ec.template_subheading('배우의 얼굴', 'white', '', 1.2), unsafe_allow_html=True)
+                    
 
 #%% Main part
 # SETTING PAGE CONFIG TO WIDE MODE
