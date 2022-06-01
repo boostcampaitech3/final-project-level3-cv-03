e90ba52c7761325ffcda5dfbe00cbae8ddcc8efe
kdg1993 <daegunkim0425@gmail.com>
2022-06-02 01:20:23 +0900

    [web] Front-end 중요 요청사항 다수 변경
    22.06.01 회의에서 나온 front-end 중요 요청사항을 적용했습니다.
    1. 파일 업로드시 업로드 가이드라인, 업로드 창 삭제
    2. 처음부터 다시하기 버튼 구현 -> 버튼 클릭시 모든 결과 삭제
    
    <주의사항>
    처음부터 다시하기 버튼 클릭시 작동하는 로직이 난해한 방식으로
    구현되어 있습니다. 작동은 하지만 필요한 부분 외 로직이 있을 수
    있기 때문에 사용에 주의하시기 바라며 로직은 이후 우선순위로 개선
    예정입니다.

--- Web_Part/front_streamlit/app.py
+++ Web_Part/front_streamlit/app.py
@@ -1,5 +1,6 @@
 from distutils.fancy_getopt import fancy_getopt
 import io
+from turtle import onclick
 import cv2
 import requests
 import base64 # ASCII string -> bytes
@@ -7,6 +8,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 from copy import deepcopy
+from time import sleep
 
 from utils import transform_image, convert_bytes_to_image
 import webbrowser, json
@@ -17,9 +19,18 @@ from front_streamlit import external_components as ec
 def uploaded_file_change_callback():
     for k in ['classification_done', 'apply_beautyGAN', 'user_face_confirm']:
         st.session_state[k] = False
+
+def add_height(n: int=1):
+    for i in range(n):
+        st.write("")
+
+def new_file():
+    st.session_state.refresh = False
+
     
 #%% Main function
 def main():
+    global uploaded_file
     # Get css
     with open('./front_streamlit/bootstrap.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -32,34 +43,41 @@ def main():
     st.markdown(ec.template_cover_heading('Look-Alike Actor'), unsafe_allow_html=True)
 
     # Initialize session state key for logic
-    session_keys = [
-        'upload_file',
-        'find_actor_clicked',
-        'apply_beautyGAN', 
-        'classification_done', 
-        'classification_img',
-        'beautyGAN_img_list',
-        'sim_actor_nm'
-        'sim_percent',
-        ]
+    def init_session_state():
+        session_keys = [
+            'refresh',
+            'uploaded_file',
+            'find_actor_clicked',
+            'apply_beautyGAN', 
+            'classification_done', 
+            'classification_img',
+            'beautyGAN_img_list',
+            'sim_actor_nm'
+            'sim_percent',
+            ]
 
-    for session_key in session_keys:
-        if session_key not in st.session_state:
-            st.session_state[session_key] = False
+        for session_key in session_keys:
+            if session_key not in st.session_state:
+                st.session_state[session_key] = False
+
+    init_session_state()
 
     # Show input guideline message
     col1, col2, col3 = st.columns(3)
     with col2:
-        st.markdown(ec.bootstrap_warning("※ 아래에 정면 사진을 올려주세요 ※"), unsafe_allow_html=True)
-        uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])        
-        st.session_state.uploaded_file = deepcopy(uploaded_file)
+        input_guide = st.empty()
+        input_guide.markdown(ec.bootstrap_warning("※ 아래에 정면 사진을 올려주세요 ※"), unsafe_allow_html=True)
+        holder = st.empty()
+        uploaded_file = holder.file_uploader("", type=["jpg", "jpeg", "png"], on_change=new_file)        
     
     # Set columns to show uploaded image and classification result image
     _, col2, col3, _ = st.columns(4)
     _, sub_col2, sub_col3, sub_col4, _ = st.columns([2, 2, 1, 1, 2])
     
     # If get user image
-    if st.session_state.uploaded_file:
+    if uploaded_file and not st.session_state.refresh:
+        st.session_state.refresh = False
+        st.session_state.uploaded_file = True
         image_bytes = uploaded_file.getvalue()
         no_makeup = transform_image(image_bytes)
         files = [
@@ -67,18 +85,15 @@ def main():
             ]
         
         # Show user uploaded image
-        with col2:
-            st.markdown(ec.template_subheading('업로드한 이미지', 'black', '#AED6F1', 1.5), unsafe_allow_html=True)
-            st.image(uploaded_file, use_column_width=True)
+        if st.session_state.uploaded_file:
+            with col2:
+                add_height(2)
+                st.markdown(ec.template_subheading('업로드한 이미지', 'black', '#AED6F1', 1.5), unsafe_allow_html=True)
+                user_img_field = st.empty()
+                user_img_field.image(uploaded_file, use_column_width=True)
+                holder.empty()
+                input_guide.empty()
         with sub_col2:
-            # if st.session_state.find_actor_clicked:
-            #     refresh_btn = st.button('새로고침')
-            #     if refresh_btn:
-            #         st.session_state.apply_beautyGAN = False
-            #         st.session_state.classification_done = False
-            #         st.session_state.uploaded_file = False
-            #         st.session_state.find_actor_clicked = False
-            # else:
             find_actor_btn = st.button('닮은 배우 찾기')
             if find_actor_btn:
                 st.session_state.find_actor_clicked = True
@@ -88,6 +103,7 @@ def main():
         if st.session_state.find_actor_clicked:
             with col3:
                 if not st.session_state.classification_done:
+                    add_height(2)
                     with st.spinner('당신과 닮은 배우를 찾는 중 입니다...'):
                         response_actor = requests.post("http://localhost:8008/actorclass", files=files)
                         st.session_state.sim_percent = response_actor.json()['percentage']
@@ -117,35 +133,44 @@ def main():
                                 unsafe_allow_html=True)
                         st.image(st.session_state.classification_img, use_column_width=True)
                         with sub_col3:
-                            apply_beautyGAN_btn = st.button('메이크업 해보기')
-                            if apply_beautyGAN_btn:
-                                st.session_state.apply_beautyGAN = True
-                        with sub_col4:
-                            refresh_btn = st.button('다시하기')
+                            refresh_btn = st.button('처음부터 다시하기')
                             if refresh_btn:
                                 st.session_state.apply_beautyGAN = False
                                 st.session_state.classification_done = False
                                 st.session_state.uploaded_file = False
                                 st.session_state.find_actor_clicked = False
+                                st.session_state.refresh = True
+                                # init_session_state()
+                                st.experimental_rerun()
+                                # st.write(st.session_state)
+                        with sub_col4:
+                            apply_beautyGAN_btn = st.button('메이크업 해보기')
+                            if apply_beautyGAN_btn:
+                                st.session_state.apply_beautyGAN = True
                 else:                        
                     # Show similar actor image
+                    add_height(2)
                     st.markdown(ec.template_subheading(
                         f'{st.session_state.sim_actor_nm}님과 {st.session_state.sim_percent*100:.1f}% 유사합니다!',
                         'black', '#D5DBDB', 1.5),
                                                         unsafe_allow_html=True)
                     st.image(st.session_state.classification_img, use_column_width=True)
                     with sub_col3:
-                        apply_beautyGAN_btn = st.button('메이크업 해보기')
-                        if apply_beautyGAN_btn:
-                            st.session_state.apply_beautyGAN = True
-                    with sub_col4:
-                        refresh_btn = st.button('다시하기')
+                        refresh_btn = st.button('처음부터 다시하기')
                         if refresh_btn:
                             st.session_state.apply_beautyGAN = False
                             st.session_state.classification_done = False
                             st.session_state.uploaded_file = False
                             st.session_state.find_actor_clicked = False
-                            
+                            st.session_state.refresh = True
+                            # init_session_state()
+                            st.experimental_rerun()
+                            # st.write(st.session_state)
+                    with sub_col4:
+                        apply_beautyGAN_btn = st.button('메이크업 해보기')
+                        if apply_beautyGAN_btn:
+                            st.session_state.apply_beautyGAN = True
+
         if st.session_state.apply_beautyGAN:
             # TODO: 이미지 View
             image_bytes = uploaded_file.getvalue() # binary 형식
@@ -169,6 +194,9 @@ def main():
             with col4:
                 st.image(st.session_state.beautyGAN_img_list[1], use_column_width=True)
                 st.markdown(ec.template_subheading('배우의 얼굴', 'white', '', 1.2), unsafe_allow_html=True)
+        else:
+            init_session_state()
+
                     
 
 #%% Main part
