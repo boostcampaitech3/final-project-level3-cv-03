3362d401c9ca72ac0eecb99f445aa15a97a37f7d
myeongu <ausrndi@gmail.com>
2022-06-01 23:14:50 +0900

    [web] logging setting

--- Web_Part/back_fastapi/app/routers/face_classifier.py
+++ Web_Part/back_fastapi/app/routers/face_classifier.py
@@ -1,3 +1,5 @@
+import os
+import time
 from fastapi import APIRouter, UploadFile, File
 from pydantic import BaseModel
 from fastapi.param_functions import Depends
@@ -5,12 +7,25 @@ from typing import List, Optional
 
 from models.efficientnet.efficientnet_model import load_model, get_prediction
 from back_fastapi.app.utils import ref_actor_image
+from Web_Part.logger import get_ml_logger
 
 router = APIRouter(
     prefix="/actorclass",
     tags=["classification"],
 )
 
+####################################
+from pathlib import Path
+
+here = Path(__file__)
+config_yaml_path = os.path.join(here.parents[2], "config.yaml")
+
+logger = get_ml_logger(
+    config_path=config_yaml_path,
+    credential_json_path="./app/online-service-logger.json", # FIXME
+    table_ref="tensile-stack-350418.online_serving_logs.inference_time", 
+)
+####################################
 
 class InferenceResult(BaseModel):
     name: str
@@ -27,8 +42,9 @@ async def inference(files: List[UploadFile] = File(...), model=Depends(load_mode
     product = ''
     # for file in files:
     image_bytes = await files[0].read()
+    start = time.time() # start time for checking inference time!
     predicted = get_prediction(model=model, image_bytes=image_bytes)
     ref_actor, inference_result = ref_actor_image(predicted)
     product = InferenceResult(name=inference_result,ref_actor =ref_actor)
-    
+    logger.info(f"Classification Inference : {time.time() - start:.5f}") # classification inference time
     return product
--- Web_Part/back_fastapi/app/routers/face_makeup.py
+++ Web_Part/back_fastapi/app/routers/face_makeup.py
@@ -1,3 +1,5 @@
+import os
+import time
 from fastapi import APIRouter, UploadFile, File
 from fastapi.param_functions import Depends
 from pydantic import BaseModel
@@ -5,10 +7,25 @@ from typing import List, Optional
 from models.beautygan.beautygan_model import get_beautygan, transfer
 import base64, io
 
+from Web_Part.logger import get_ml_logger
+
 router = APIRouter(
     prefix="/beauty",
     tags=["beautygan"],
 )
+
+####################################
+from pathlib import Path
+
+here = Path(__file__)
+config_yaml_path = os.path.join(here.parents[2], "config.yaml")
+
+logger = get_ml_logger(
+    config_path=config_yaml_path,
+    credential_json_path="./app/online-service-logger.json", # FIXME
+    table_ref="tensile-stack-350418.online_serving_logs.inference_time", 
+)
+####################################
 
 class TransferImage(BaseModel):
     result: Optional[List[str]]
@@ -25,8 +42,11 @@ async def make_transfer(
     image_bytes = await files[0].read() # user image
     ref_bytes = await files[1].read() # refer image
 
+    start = time.time() # start time for checking inference time!
+
     # np.ndarray -> PIL 이미지 -> ASCII코드로 변환된 bytes 데이터(str)
     transfer_result, transfer_refer = transfer(sess, graph, image_bytes, ref_bytes)
     product = TransferImage(result=[transfer_result, transfer_refer])
 
+    logger.info(f"BeautyGAN Inference : {time.time() - start:.5f}") # beautygan inference time
     return product
