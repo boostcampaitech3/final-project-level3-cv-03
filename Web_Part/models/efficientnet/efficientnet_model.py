33dc4975602e91e3d41c2ef049168be006c346bd
ptwd <zaq5526@gmail.com>
2022-06-02 06:26:36 +0000

    [Backend] Add face process codes
    얼굴의 갯수가 0개, 1개이상일 때 처리하는 코드를 추가하였습니다.

--- Web_Part/models/efficientnet/efficientnet_model.py
+++ Web_Part/models/efficientnet/efficientnet_model.py
@@ -12,13 +12,16 @@ import io, cv2
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+
+from back_fastapi.app.utils import from_image_to_bytes
+from PIL import Image
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 import insightface
 from insightface.utils.face_align import *
 from insightface.utils.face_align import norm_crop as norm_crop
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
 
 CROPPED_IMG_SIZE = 1024 ##
 insightface.utils.face_align.src_map = {
@@ -27,7 +30,7 @@ insightface.utils.face_align.src_map = {
     1024 : insightface.utils.face_align.src * 1024 / 112
 }
 
-app = FaceAnalysis(providers=['CPUExecutionProvider'])
+app = FaceAnalysis(allowed_modules=['detection'], providers=['CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(256, 256))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -67,7 +70,17 @@ def transform_image(image_bytes: bytes, get_box: Boolean=False) -> torch.Tensor:
     img = cv2.imdecode(img_, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
     boxes_raw = app.get(img)
-    boxes = boxes_raw[0]
+    
+    max_area = 0
+    max_index = -1
+    for index, box in enumerate(boxes_raw):
+        bbox = box['bbox']
+        if max_area < (bbox[2]-bbox[0]) * (bbox[3]-bbox[1]):
+            max_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
+            max_index = index
+    boxes = boxes_raw[max_index]
+
+
     img = norm_crop(img = img, landmark = boxes['kps'], image_size = CROPPED_IMG_SIZE, mode = 'NOMODE!') ## mode = 'arcface'
     img /= 255.0
     test_transform = A.Compose([A.Resize(380, 380), ToTensorV2()])
@@ -75,3 +88,34 @@ def transform_image(image_bytes: bytes, get_box: Boolean=False) -> torch.Tensor:
         return test_transform(image=img)["image"].unsqueeze(0), boxes_raw
     else:
         return test_transform(image=img)["image"].unsqueeze(0)
+
+
+def convert_image(image_bytes: bytes):
+    img_ = np.frombuffer(image_bytes, dtype=np.uint8)
+    img = cv2.imdecode(img_, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
+    boxes_raw = app.get(img)
+    if len(boxes_raw) == 0:
+
+        return len(boxes_raw), ''
+
+    elif len(boxes_raw) >= 1:
+        max_area = 0
+        max_index = -1
+        for index, box in enumerate(boxes_raw):
+            bbox = box['bbox']
+            if max_area < (bbox[2]-bbox[0]) * (bbox[3]-bbox[1]):
+                max_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
+                max_index = index
+        boxes = boxes_raw[max_index]
+
+        img = norm_crop(img = img, landmark = boxes['kps'], image_size = CROPPED_IMG_SIZE, mode = 'NOMODE!') ## mode = 'arcface'
+        img = cv2.resize(img, (380, 380), interpolation=cv2.INTER_AREA)
+        img = img.astype(np.uint8)
+        pil_image = Image.fromarray(img)
+        output_img_bytes = from_image_to_bytes(pil_image)
+
+        return len(boxes_raw), output_img_bytes
+    else:
+        print("###############HEy!!!!!!")
+        return "anything"
