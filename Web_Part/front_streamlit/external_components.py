c0f7bc74296f4eed70a01b7154df555bd0da4be5
hyunzzin <forinpea121@gmail.com>
2022-06-08 05:51:32 +0000

    [web] back up

--- Web_Part/front_streamlit/external_components.py
+++ Web_Part/front_streamlit/external_components.py
@@ -83,7 +83,7 @@ def bootstrap_warning(text: str):
 def bootstrap_navbar():
     return """
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">배우고 싶니?</a>
+        <a class="navbar-brand like-a-actor" href="#">배우고 싶니?</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
